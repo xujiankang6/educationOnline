49dc5ef2ad6a1b5ad1a13e9eda8d7d016e273e65
xujiankang6 <1241450485@qq.com>
2019-05-11 21:55:32 +0800

    add some src/main/java and sql

---
+++ src/main/java/com/bdqn/jiankang/bean/Role.java
@@ -0,0 +1,35 @@
+package com.bdqn.jiankang.bean;
+
+import java.util.List;
+
+public class Role
+{
+
+    private int rid;
+    private String rname;
+    private List<Permission> permissions;
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+}
